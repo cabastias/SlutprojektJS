4a7faf0d975459135e3248147037efb35fafe302
Cassandra Bastias <cassandra.bastias@hotmail.com>
2021-03-04 17:20:24 +0100

    Login almost done

--- app.js
+++ app.js
@@ -12,6 +12,28 @@ app.use(express.urlencoded({ extended: true }));
     app.get('/login', function(req, res){
         
     });
+    app.post('/login/auth', function(req, res){
+        const user_input = {
+            email: req.body.email,
+            password: req.body.password,
+        }
+        db.findOne({email: user_input.email}).exec((err, user_data) => {
+           if(user_data){
+            bcrypt.compare(user_input.password, user_data.password).then(function(result) {
+                if(result){
+                    res.status(200).send({'respons': "You have been logged in!"})
+                }else{
+                    res.status(401).send({'respons': "Wrong password or email!"})
+                }
+            });
+           }else{
+                res.status(401).send({'respons': "Wrong password or email!"})
+           }
+            
+            
+        });
+    });
+
     app.post('/register', function(req, res) {
         const newUser = {
             email: req.body.email,
@@ -28,6 +50,7 @@ app.use(express.urlencoded({ extended: true }));
             }
         })
     });
+    
     
 app.listen(8090, () => {
     console.log("Server running on port 8090")
