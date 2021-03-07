e105fe88eade1dcb03ceae742d3acef1bf114f3a
cassandra <cassandra.bastias@hotmail.com>
2021-03-07 16:02:27 +0100

    jwt done

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const express = require('express')
+const jwt = require('jsonwebtoken');
+const accessTokenSecret = 'youraccesstokensecret';
+const refreshTokenSecret = 'secret';
+const refreshTokens = [];
 const bcrypt = require('bcrypt')
 var Datastore = require('nedb')
   , db = new Datastore({ filename: 'data.db', autoload: true });
@@ -8,20 +12,41 @@ const saltRounds = 10;
 const salt = bcrypt.genSaltSync(saltRounds);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+const authenticateJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization;
 
-    app.get('/login', function(req, res){
-        
-    });
+    if (authHeader) {
+        const token = authHeader.split(' ')[1];
+
+        jwt.verify(token, accessTokenSecret, (err, user) => {
+            if (err) {
+                return res.sendStatus(403);
+            }
+
+            req.user = user;
+            next();
+        });
+    } else {
+        res.sendStatus(401);
+    }
+}
+
+
     app.post('/login/auth', function(req, res){
         const user_input = {
             email: req.body.email,
             password: req.body.password,
         }
         db.findOne({email: user_input.email}).exec((err, user_data) => {
+            console.log(user_data);
            if(user_data){
             bcrypt.compare(user_input.password, user_data.password).then(function(result) {
                 if(result){
-                    res.status(200).send({'respons': "You have been logged in!"})
+                    const accessToken = jwt.sign({ email: user_data.email }, accessTokenSecret, { expiresIn: '20m' });
+                   const refreshToken = jwt.sign({ email: user_data.email }, refreshTokenSecret);
+
+        refreshTokens.push(refreshToken);
+                    res.send({'respons': "You have been logged in!", 'token': accessToken})
                 }else{
                     res.status(401).send({'respons': "Wrong password or email!"})
                 }
@@ -33,14 +58,15 @@ app.use(express.urlencoded({ extended: true }));
             
         });
     });
-
     app.post('/register', function(req, res) {
+        console.log(req.body);
         const newUser = {
             email: req.body.email,
             password: bcrypt.hashSync(req.body.password, salt),
             name: req.body.name,
             role: "user"
         }
+        console.log(newUser)
         db.find({ "email": newUser.email}, function (err,docs){
             if(docs.length > 0){
                 res.status(400).send({ respons: "User already exists!" });
@@ -49,8 +75,10 @@ app.use(express.urlencoded({ extended: true }));
                 res.json({ respons: "User has been registered!" });
             }
         })
+    });  
+    app.get('/test', authenticateJWT, (req, res) => {
+        res.json("Test");
     });
-    
     
 app.listen(8090, () => {
     console.log("Server running on port 8090")
