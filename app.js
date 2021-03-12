660d78a910cad5bc4b53b383d5654a978f609b7e
cassandra <cassandra.bastias@hotmail.com>
2021-03-12 14:44:44 +0100

    new app.js file

--- app.js
+++ app.js
@@ -99,6 +99,10 @@ const authenticateJWT = (req, res, next) => {
             }
         })
     });
+    // delete books here
+    app.delete('/books', (req, res) => { 
+        res.send("DELETE Request Called") 
+      }) 
     // only test stuff 
     app.get('/test', authenticateJWT, (req, res) => {
         res.json("Test");
@@ -107,7 +111,46 @@ const authenticateJWT = (req, res, next) => {
     app.get('/books', authenticateJWT, (req, res) => {
         res.json("books");
     });
-  
+    // delete book
+    app.get('/books', authenticateJWT, (req, res) => {
+        res.json(" delete books");
+    });
+  // token here
+    app.post('/token', (req, res) => {
+        const { token } = req.body;
+    
+        if (!token) {
+            return res.sendStatus(401);
+        }
+    
+        if (!refreshTokens.includes(token)) {
+            return res.sendStatus(403);
+        }
+    
+        jwt.verify(token, refreshTokenSecret, (err, user) => {
+            if (err) {
+                return res.sendStatus(403);
+            }
+    
+            const accessToken = jwt.sign({ username: user.username, role: user.role }, accessTokenSecret, { expiresIn: '20m' });
+    
+            res.json({
+                accessToken
+            });
+        });
+    });
+
+       // logout here 
+       app.post('/logout', (req, res) => {
+        const { token } = req.body;
+        refreshTokens = refreshTokens.filter(t=> t !== token);
+
+    
+        res.send("Logout successful");
+    });
+ 
+
+
 // startar servern
 app.listen(8090, () => {
     console.log("Server running on port 8090")
