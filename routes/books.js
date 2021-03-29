8a2d2e9313a3d364b779403600f06c9968bdb77b
cassandra <cassandra.bastias@hotmail.com>
2021-03-29 09:04:31 +0200

    routes and books database

--- routes/books.js
+++ routes/books.js
@@ -1,31 +1,33 @@
 const express = require('express')
-const passport = require('passport')
-const jwt = require('jsonwebtoken');
-const books = require('../routes/books')
+const books = require('/routes/books')
+const bcrypt = require('bcrypt')
+var Datastore = require('nedb')
+ db = new Datastore({ filename: 'books.db', autoload: true });
 
+ // routes 
 router.get('/', async function (req, res) {
     const books = await product.find().exec()
     res.send(books)
 })
 router.post('/', function (req, res) {
     books.create(req.body)
-    res.send('POST Request accepted.')
+    res.send('POST accepted.')
 })
 router.patch('/:id', async function (req, res) {
     const result = await books.updateMany({
         "_id": String(req.params.id)
     }, { $set: req.body })
-    res.send('PATCH Request accepted.')
+    res.send('PATCH accepted.')
 })
 router.delete('/:id', async function (req, res) {
     const result = await books.deleteMany({
         "_id": String(req.params.id)
     })
-    res.send('DELETE Request accepted.')
+    res.send('DELETE accepted.')
 })
 module.exports = router
 
 // startar servern
-app.listen(8090, () => {
-    console.log("Server running on port 8090")
+app.listen(8070, () => {
+    console.log("Server running on port 8070")
 });
