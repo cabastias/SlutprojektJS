0a741760f1664873e3d35458cc1f1b731588865a
cassandra <cassandra.bastias@hotmail.com>
2021-04-06 10:18:13 +0200

    added models and routes

--- routes/books.js
+++ routes/books.js
@@ -1,33 +1,61 @@
-const express = require('express')
-const books = require('/routes/books')
-const bcrypt = require('bcrypt')
-var Datastore = require('nedb')
- db = new Datastore({ filename: 'books.db', autoload: true });
+// routes/api/books.js
 
- // routes 
-router.get('/', async function (req, res) {
-    const books = await product.find().exec()
-    res.send(books)
-})
-router.post('/', function (req, res) {
-    books.create(req.body)
-    res.send('POST accepted.')
-})
-router.patch('/:id', async function (req, res) {
-    const result = await books.updateMany({
-        "_id": String(req.params.id)
-    }, { $set: req.body })
-    res.send('PATCH accepted.')
-})
-router.delete('/:id', async function (req, res) {
-    const result = await books.deleteMany({
-        "_id": String(req.params.id)
-    })
-    res.send('DELETE accepted.')
-})
-module.exports = router
+const express = require('express');
+const router = express.Router();
 
-// startar servern
-app.listen(8070, () => {
-    console.log("Server running on port 8070")
+// Load Book model
+const Book = require('../../models/Book');
+
+// @route GET api/books/test
+// @description tests books route
+// @access Public
+router.get('/test', (req, res) => res.send('book route testing!'));
+
+// @route GET api/books
+// @description Get all books
+// @access Public
+router.get('/', (req, res) => {
+  Book.find()
+    .then(books => res.json(books))
+    .catch(err => res.status(404).json({ nobooksfound: 'No Books found' }));
 });
+
+// @route GET api/books/:id
+// @description Get single book by id
+// @access Public
+router.get('/:id', (req, res) => {
+  Book.findById(req.params.id)
+    .then(book => res.json(book))
+    .catch(err => res.status(404).json({ nobookfound: 'No Book found' }));
+});
+
+// @route GET api/books
+// @description add/save book
+// @access Public
+router.post('/', (req, res) => {
+  Book.create(req.body)
+    .then(book => res.json({ msg: 'Book added successfully' }))
+    .catch(err => res.status(400).json({ error: 'Unable to add this book' }));
+});
+
+// @route GET api/books/:id
+// @description Update book
+// @access Public
+router.put('/:id', (req, res) => {
+  Book.findByIdAndUpdate(req.params.id, req.body)
+    .then(book => res.json({ msg: 'Updated successfully' }))
+    .catch(err =>
+      res.status(400).json({ error: 'Unable to update the Database' })
+    );
+});
+
+// @route GET api/books/:id
+// @description Delete book by id
+// @access Public
+router.delete('/:id', (req, res) => {
+  Book.findByIdAndRemove(req.params.id, req.body)
+    .then(book => res.json({ mgs: 'Book entry deleted successfully' }))
+    .catch(err => res.status(404).json({ error: 'No such a book' }));
+});
+
+module.exports = router;
